eace7dc0c991b8f51499b00d8428674173d56999
Prathik <prathik.m@tala.co>
2019-06-07 08:07:55 +0530

    feat: 🎸 Add reschedule support in service layer
    Reschedule moves the current topic to 3 days ahead.

--- service/spacedrepitition.go
+++ service/spacedrepitition.go
@@ -8,9 +8,10 @@ import (
 )
 
 type Topic struct {
-	Title string
-	Times int
+	Title   string
+	Times   int
 	NextRun time.Time
+	Id      int64
 }
 
 type SpacedRepetition struct {
@@ -21,7 +22,7 @@ func (sr SpacedRepetition) Init()  {
 	sqlStmt := `
 	create table if not exists sr_data 
 	(id integer not null primary key, title text, times int,
-	next_run datetime(9));
+	next_run datetime);
 	`
 
 	_, createErr := sr.SqlDataBase.Exec(sqlStmt)
@@ -38,7 +39,7 @@ insert into sr_data (title, times, next_run) values ("%s", 0, "%s")
 `
 	_, err := sr.SqlDataBase.Exec(
 		fmt.Sprintf(insertStatementTemplate, topic.Title, time.Now().Add(
-			time.Hour * 1).String()))
+			time.Hour * 1).Format(time.RFC3339)))
 	if err != nil {
 		panic(err)
 	}
@@ -46,11 +47,11 @@ insert into sr_data (title, times, next_run) values ("%s", 0, "%s")
 
 //GetTopicNow Get a topic from persistence layer
 func (sr SpacedRepetition) GetTopicNow() (topic *Topic) {
-	getOldestTemplate := `SELECT id, title, times FROM sr_data
+	getOldestTemplate := `SELECT id, title, times, next_run FROM sr_data
 WHERE next_run <= "%s" ORDER BY next_run ASC LIMIT 1
 `
 	rows, err := sr.SqlDataBase.Query(fmt.Sprintf(getOldestTemplate,
-		time.Now().String()))
+		time.Now().Format(time.RFC3339)))
 
 	if err != nil {
 		panic(err)
@@ -59,18 +60,22 @@ WHERE next_run <= "%s" ORDER BY next_run ASC LIMIT 1
 	defer rows.Close()
 
 	for rows.Next() {
-		var id int
+		var id int64
 		var title string
 		var times int
+		var nextRun string
 
-		err := rows.Scan(&id, &title, &times)
+		err := rows.Scan(&id, &title, &times, &nextRun)
 		if err != nil {
 			panic(err)
 		}
 
+		nextRunTime, _ := time.Parse(time.RFC3339, nextRun)
 		topic = &Topic{
+			Id: id,
 			Title: title,
 			Times: times,
+			NextRun: nextRunTime,
 		}
 	}
 
@@ -79,4 +84,13 @@ WHERE next_run <= "%s" ORDER BY next_run ASC LIMIT 1
 
 //RescheduleTopic Updates the next run time in the persistence layer
 func (sr SpacedRepetition) RescheduleTopic(topic *Topic) {
+	timeFactor := topic.Times + 1
+
+	nextRunTime := topic.NextRun.Add(
+		time.Hour * time.Duration(24 * 3 * timeFactor))
+
+	updateStatement := `UPDATE sr_data set next_run = "%s" WHERE id = %d`
+
+	_, _ = sr.SqlDataBase.Exec(fmt.Sprintf(updateStatement, nextRunTime.Format(
+		time.RFC3339), topic.Id))
 }
--- service/spacedrepitition_test.go
+++ service/spacedrepitition_test.go
@@ -27,7 +27,8 @@ func TestSpacedRepetition_Add(t *testing.T) {
 		Title: "Test",
 	})
 
-	rows, err := db.Query("select id, title, times, next_run from sr_data")
+	rows, err := db.Query(`select id, title, times, next_run
+from sr_data`)
 
 	if err != nil {
 		t.Error(err)
@@ -58,11 +59,12 @@ func TestSpacedRepetition_Add(t *testing.T) {
 			t.Fail()
 		}
 
-		nr, _ := time.Parse("2019-06-06 21:42:16.790853 +0530 IST m=+3600.003102276",
+		nr, _ := time.Parse(time.RFC3339,
 			nextRun)
 
-		if nr.Before(time.Now().Add(time.Hour * 1)) == false {
-			t.Fail()
+		if nr.Before(time.Now().Add(time.Minute * 59)) == true {
+			t.Errorf("%s is before 1 hour. Base time %s", nr.String(),
+				nextRun)
 		}
 
 		if nr.After(time.Now().Add(time.Minute * 61)) == true {
@@ -121,9 +123,9 @@ func TestSpacedRepetition_GivenTopicInDb_WhenGetCalled_ReturnTopTopic(t *testing
 	insertStatementTemplate := `
 insert into sr_data (title, times, next_run) values ("%s", 0, "%s")
 `
-	_, _ = db.Exec(fmt.Sprintf(insertStatementTemplate, "Test", time.Now().String()))
+	_, _ = db.Exec(fmt.Sprintf(insertStatementTemplate, "Test", time.Now().Format(time.RFC3339)))
 	_, _ = db.Exec(fmt.Sprintf(insertStatementTemplate, "Test Old", time.Now().Add(
-		time.Hour * time.Duration(-1)).String()))
+		time.Hour * time.Duration(-1)).Format(time.RFC3339)))
 
 	topic := sr.GetTopicNow()
 
@@ -150,6 +152,89 @@ func TestSpacedRepetition_GivenNoTopicInDb_WhenGetCalled_ReturnTopTopic(t *testi
 
 	if topic != nil {
 		t.Error("Topic is present.")
+	}
+}
+
+func TestSpacedRepetition_GivenTopicId_WhenRescheduleCalled_UpdateNextRunTime(t *testing.T)  {
+	db := getDb(t)
+	defer db.Close()
+	defer func(){
+		_ = os.Remove("./sr.db")
+	}()
+
+	sr := SpacedRepetition{
+		SqlDataBase:db,
+	}
+
+	// Create table
+	sr.Init()
+
+	insertStatementTemplate := `
+insert into sr_data (title, times, next_run) values ("%s", 0, "%s")
+`
+	_, _ = db.Exec(fmt.Sprintf(insertStatementTemplate, "Test", time.Now().Format(
+		time.RFC3339)))
+
+	topic := sr.GetTopicNow()
+
+	if topic == nil {
+		t.Errorf("No topic fetched.")
+	}
+
+	if topic.Title != "Test" {
+		t.Error("Invalid topic.")
+	}
+
+	sr.RescheduleTopic(topic)
+
+	rows, err := db.Query("select id, title, times, next_run from sr_data")
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	defer rows.Close()
+
+	var count = 0
+
+	for rows.Next() {
+		var id int
+		var title string
+		var times int
+		var nextRun string
+		err := rows.Scan(&id, &title, &times, &nextRun)
+		if err != nil {
+			t.Error(err)
+		}
+		if title != "Test" {
+			t.Fail()
+		}
+
+		if id != 1 {
+			t.Fail()
+		}
+
+		if times != 0 {
+			t.Fail()
+		}
+
+		nr, _ := time.Parse(time.RFC3339,
+			nextRun)
+
+		// Scheduled 3 days ahead from now.
+		if nr.Before(time.Now().Add(time.Hour * 24 * 2)) == true {
+			t.Errorf("Next run is before two days %s", nr.String())
+		}
+
+		if nr.After(time.Now().Add(time.Hour * 24 * 4)) == true {
+			t.Fail()
+		}
+
+		count++
+	}
+
+	if count == 0 {
+		t.Error("No rows")
 	}
 }
 
